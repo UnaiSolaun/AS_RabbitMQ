7d3a1e63c3727aacbe8727f67c839decbde1383a
UnaiSolaun <unai.solaun@zaraobe.net>
2022-12-06 13:52:42 +0100

    Act volumen y receive.py

--- receiver/receive.py
+++ receiver/receive.py
@@ -2,11 +2,11 @@ import pika
 
 def callback(ch, method, properties, body):
     print(" [x] Received %s" %body)
+    connection.close()
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
 channel = connection.channel()
 channel.queue_declare(queue='hello')
 channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
-print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
 
